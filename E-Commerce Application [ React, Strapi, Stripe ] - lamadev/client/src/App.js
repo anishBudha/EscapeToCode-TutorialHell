1c02fbd300818b8ccf5d43ce6882e17c74d9f01a
anishBudha <anishbudha5628@gmail.com>
2024-01-22 23:27:12 -0500

    working on navbar

--- E-Commerce Application [ React, Strapi, Stripe ] - lamadev/client/src/App.js
+++ E-Commerce Application [ React, Strapi, Stripe ] - lamadev/client/src/App.js
@@ -1,32 +1,53 @@
-import {
-  // from react-router-dom docs
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+// importing pages: home, products and product
+import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
+import Products from "./pages/Products/Products.jsx";
+
+// importing navbar and footer
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+// for navbar - outlet(which will render our pages) - footer
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <span>Home</span>,
-  },
-  {
-    path: "/products/:id",
-    element: <span>Category</span>,
-  },
-  {
-    path: "/product/:id",
-    element: <span>Product</span>,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+    ],
   },
 ]);
 
-const App = () => {
+function App() {
   return (
     <div>
       <RouterProvider router={router} />
     </div>
   );
-};
+}
 
 export default App;
---
+++ E-Commerce Application [ React, Strapi, Stripe ] - lamadev/client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+// material ui icons imports
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left">
+          <div className="item">
+            <img src="/img/en.png" alt="ameican flaf" />
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <span>USD</span>
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <span>USD</span>
+          </div>
+        </div>
+        <div className="center"></div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
