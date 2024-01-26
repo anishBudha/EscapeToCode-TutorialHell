5e54666773216902829b2d151e3f56593bc4e044
anishBudha <anishbudha5628@gmail.com>
2024-01-25 19:36:57 -0500

    slider completed

--- E-Commerce Application [ React, Strapi, Stripe ] - lamadev/client/src/components/Slider/Slider.jsx
+++ E-Commerce Application [ React, Strapi, Stripe ] - lamadev/client/src/components/Slider/Slider.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Slider.scss";
 
 // material ui icons
@@ -6,6 +6,17 @@ import EastOutlinedIcon from "@mui/icons-material/EastOutlined";
 import WestOutlinedIcon from "@mui/icons-material/WestOutlined";
 
 const Slider = () => {
+  // useState for the slides
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  // functions for the buttons
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? 2 : (prev) => prev - 1);
+  };
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === 2 ? 0 : (prev) => prev + 1);
+  };
+
   const data = [
     "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
     "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
@@ -13,16 +24,19 @@ const Slider = () => {
   ];
   return (
     <div className="slider">
-      <div className="container">
+      <div
+        className="container"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
         <img src={data[0]} alt="" />
         <img src={data[1]} alt="" />
         <img src={data[2]} alt="" />
       </div>
       <div className="icons">
-        <div className="icon">
+        <div className="icon" onClick={prevSlide}>
           <WestOutlinedIcon />
         </div>
-        <div className="icon">
+        <div className="icon" onClick={nextSlide}>
           <EastOutlinedIcon />
         </div>
       </div>
